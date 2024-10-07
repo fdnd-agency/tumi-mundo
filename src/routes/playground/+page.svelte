b68e0d4efa60003067c1fc952fc056011a6b4d74
OvenMagnetron <jessekramer15@gmail.com>
2024-10-07 14:04:18 +0200

    JK - Added new connection

---
+++ src/routes/playground/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+    /** @type {import('./$types').PageData} */
+    export let data;
+    
+</script>
+
+<section>
+    <article>
+        {#if data.accounts && data.accounts.length > 0}
+        {#each data.accounts as account}
+            <ul>
+                <li>{account.id}</li>
+            </ul>
+        {/each}
+    {:else}
+        <!-- This will show if no people are available -->
+        <p>Er zitten geen studenten in deze squad</p>
+    {/if}
+    </article>
+</section>
