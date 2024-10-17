497bf8fd6adc433f2eee0d9f81f3ab925e8d1e87
Akikosophia <144008863+Akikosophia@users.noreply.github.com>
2024-10-17 13:10:02 +0200

    AS - Liked story component
    Make component of the liked story and some adjustments

--- src/routes/playlist/+page.svelte
+++ src/routes/playlist/+page.svelte
@@ -31,6 +31,9 @@
 //       goToSlide(currentSlide - 1);
 //     }
 
+
+import Likedstory from '../../components/liked_story.svelte'
+
   // Maak een variabele voor de slide met daarin een array aan voor de verschillende slides 
   let slides = ['Slide 1', 'Slide 2', 'Slide 3'];
   let currentSlide = 0; 
@@ -489,105 +492,8 @@
       </div>
     </div> 
 
-    <section class="content">
-      <h2>Liked playlist</h2>
-
-      <article class="pinguin-kaart">
-        <article class="pinguin-card">
-          <img src="/Frame 27 (1).svg" class="pinguin" alt="pinguin" />
-          <p class="pinguin-tekst">Penguin activities!</p>
-
-          <div class="pinguin-icon">
-            <img
-              src="/Group 316.svg"
-              class="playbutton"
-              alt="play-button"
-            />
-            <p class="tijd">31 min. 55 sec</p>
-
-            <img src="/Heart.svg" class="heart-icon" alt="heart-icon" />
-          </div>
-        </article>
-
-        <article class="pinguin-card">
-          <img src="/Frame 27 (4).svg" class="pinguin" alt="pinguin" />
-          <p class="pinguin-tekst">
-            Christmas stories, for the festive season.
-          </p>
-
-          <div class="pinguin-icons">
-            <img
-              src="/Group 316.svg"
-              class="playbutton"
-              alt="play-button"
-            />
-            <p class="tijd">31 min. 55 sec</p>
-
-            <img src="/Heart.svg" class="heart-icon" alt="heart-icon" />
-          </div>
-        </article>
-      </article>
-    </section>
-
-    <section class="last-content">
-      <h2>Suggested playlist</h2>
-
-      <article class="pinguin-kaart">
-        <article class="pinguin-card">
-          <img src="/Frame 27 (1).svg" class="pinguin" alt="pinguin" />
-          <p class="pinguin-tekst">Penguin activities!</p>
-
-          <div class="pinguin-icon">
-            <img
-              src="/Group 316.svg"
-              class="playbutton"
-              alt="play-button"
-            />
-            <p class="tijd">31 min. 55 sec</p>
-
-            <img src="/Icon.svg" class="empty-heart" alt="empty-heart-icon" />
-          </div>
-        </article>
-
-        <article class="pinguin-card">
-          <img src="/Frame 27 (4).svg" class="pinguin" alt="pinguin" />
-          <p class="pinguin-tekst">
-            Christmas stories, for the festive season.
-          </p>
-
-          <div class="pinguin-icons">
-            <img
-              src="/Group 316.svg"
-              class="playbutton"
-              alt="play-button"
-            />
-            <p class="tijd">31 min. 55 sec</p>
-
-            <img src="/Icon.svg" class="empty-heart" alt="empty-heart-icon" />
-          </div>
-        </article>
-
-        <article class="pinguin-card">
-          <img src="/Frame 27 (4).svg" class="pinguin" alt="pinguin" />
-          <p class="pinguin-tekst">
-            Christmas stories, for the festive season.
-          </p>
-
-          <div class="pinguin-icons">
-            <img
-              src="/Group 316.svg"
-              class="playbutton"
-              alt="play-button"
-            />
-            <p class="tijd">31 min. 55 sec</p>
-
-            <img src="/Icon.svg" class="empty-heart" alt="empty-heart-icon" />
-          </div>
-        </article>
-
-
-      </article>
-    </section>
+  <Likedstory/>
+  
   </main>
 </body>
 
@@ -978,86 +884,11 @@
     transition: 0.3s ease;
   }
 
-  section.last-content {
-    transform: translateX(65px);
-  }
-
-  article.pinguin-kaart {
-    display: flex;
-    flex-direction: row;
-    gap: 10px;
-    margin-right: 55px;
-    margin-bottom: 30px;
-  }
-
-  article.pinguin-card {
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
-    width: 105px;
-    height: 170px;
-    border-radius: 10px;
-    background-color: white;
-    padding: 10px;
-    transform: translateX(-14px);
-}
-
-  img.pinguin {
-    width: 105px;
-    height: 95px;
-    object-fit: cover;
-    border-radius: 10px;
-    margin-bottom: 3px;
-  }
-
-  .pinguin-tekst {
-    font-size: var(--card-description-font-size);
-    text-align: left;
-    color: var(--card-font-color);
-    line-height: 16.5px;
-    margin: 0;
-    margin-bottom: 10px;
-  }
-
-  .pinguin-icon {
-    display: flex;
-    align-items: center;
-    justify-content: space-around;
-    margin-top: 15px;
-}
-  
-  .pinguin-icons {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    margin-bottom: 10px;
-  }
-
-  img.empty-heart {
-    width: 22px;
-    height: 30px;
-    transform: translateX(5px);
-  }
-
-  img.playbutton {
-    width: 24px;
-    height: 24px;
-    transform: translateX(-2px);
-  }
-
-  img.heart-icon  {
-    width: 28px;
-    height: 30px;
-    transform: translateX(5px);
-  }
-
   img.playlist-icon {
     margin-top: 22px;
     margin-bottom: 8px;
   }
   
-  p.tijd {
-    transform: translateX(2px);
-  }
+
 
 </style>
