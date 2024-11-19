e2cbdf49487c255126564fdb954aed8b32e9ce57
lemuelmgsn <magsinolemuel68@gmail.com>
2024-11-19 01:00:59 +0100

    LM - added function to search component
    still needs to be made into component

--- src/routes/languages/+page.js
+++ src/routes/languages/+page.js
@@ -5,11 +5,6 @@ import { readItems } from '@directus/sdk';
 export async function load({ fetch }) {
 	const directus = getDirectusInstance(fetch);
 	return {
-		accounts: await directus.request(readItems('tm24_account')),
-        buddys: await directus.request(readItems('tm24_buddy')),
-        profiles: await directus.request(readItems('tm24_profile')),
-        stories: await directus.request(readItems('tm24_story')),
-        personal_playlists: await directus.request(readItems('tm24_personal_playlist')),
         languages: await directus.request(readItems('tm24_language')),
 
 	};
