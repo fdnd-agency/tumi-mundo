6e66000801f0887daafe89e03e5184b86fc6d932
OvenMagnetron <jessekramer15@gmail.com>
2024-11-20 22:22:15 +0100

    JK - Rafactored story component

--- src/components/story.svelte
+++ src/components/story.svelte
@@ -1,6 +1,6 @@
 <script>
     let stories = [
-        { title: 'Story One', summary: 'This is the description of the story. Some titles may be a little longer than usual. We need to build in an line-clamp for that.', image: '/stories/Story-pengiun.png', playtime: '2 min. 5 sec', language: 'English'},
+        { title: 'Story One', summary: 'This is the description of the story. Some titles may be a little longer than usual. We need to build in an line-clamp for that. Now it will stop it from overflowing, cool huh?', image: '/stories/Story-pengiun.png', playtime: '2 min. 5 sec', language: 'English'},
         { title: 'Story Two', summary: 'This is the description of the story.', image: '/stories/Story-trees.png', playtime: '3 min. 6 sec', language: 'Dutch'},
         { title: 'Story Three', summary: 'This is the description of the story.', image: '/stories/Story-turtle.png', playtime: '1 min. 15 sec', language: 'French'},
         { title: 'Story Four', summary: 'This is the description of the story.', image: '/stories/Story-pengiun.png', playtime: '4 min. 53 sec', language: 'German'}
@@ -8,36 +8,32 @@
 </script>
 
 <section>
-{#each stories as story}
-    <article class="story-container">
-        <div class=story-image>
-            <img src="{story.image}" alt="">
-        </div>
-        <div class="story-title">
-            <h6>{story.title}</h6>
-        </div>
-        <div class="story-summary">
-            <p>{story.summary}</p>
-        </div>
-        <div class="story-language">
-            <img src="/languages/{story.language}.svg" alt="{story.language} flag">
-        </div>
-        <div class="story-meta">
-            <div class="story-playtime">
-                <img src="/icons/story-playtime.svg" alt="Icon for playtime">
-                <p>{story.playtime}</p>
-            </div>
-            <div class="story-icons">
-                <img src="/icons/download-icon.svg" alt="Icon for download">
-                <img src="/icons/add-to-playlist.svg" alt="Icon for adding to playlist">
-            </div>
-        </div>
-    </article>
-{/each}
+    <ul>
+        {#each stories as story}
+            <li class="story-container">
+                <div class="story-image flex-items">
+                    <img src="{story.image}" alt="">
+                </div>
+                <h2 class="story-title">{story.title}</h2>
+                <div class="story-language flex-items">
+                    <img src="/languages/{story.language}.svg" alt="{story.language} flag">
+                </div>
+                <p class="story-summary">{story.summary}</p>
+                <div class="story-playtime flex-items">
+                    <img src="/icons/story-playtime.svg" alt="Icon for playtime">
+                    <p>{story.playtime}</p>
+                </div>
+                <div class="story-icons flex-items">
+                    <img src="/icons/download-icon.svg" alt="Icon for download">
+                    <img src="/icons/add-to-playlist.svg" alt="Icon for adding to playlist">
+                </div>
+            </li>
+        {/each}
+    </ul>
 </section>
 
 <style>
-    section {
+    ul {
         padding: 20px 0 20px 0;
         display: flex;
         flex-direction: column;
@@ -45,7 +41,7 @@
         align-items: center;
     }
 
-    article.story-container {
+    li {
         width: 360px;
         max-height: 85px;
         display: grid;
@@ -57,77 +53,56 @@
         border-radius: 4px;
     }
 
-    .story-image { 
-        grid-area: 1 / 1 / 4 / 3;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        padding-right: 10px;
-    }
-
-    .story-image img {
-        max-width: 60px;
-        max-height: 64px;
-    }
-
-    .story-title{ 
-        grid-area: 1 / 3 / 2 / 6; 
-    }
-
-    .story-title h6 {
+    .story-title {
         font-size: 10px;
-        font-weight: bold;
+        font-weight: 600;
+        grid-area: 1 / 3 / 2 / 6;
     }
 
-    .story-summary{ 
-        grid-area: 2 / 3 / 3 / 7; 
+    p {
+        font-size: 8px;
+    }
+
+    img {
+        max-width: 18px;
+    }
+
+    .story-image {
+        grid-area: 1 / 1 / 4 / 2;
+    }
+
+    .story-image img{
+        max-width: 60px;
+    }
+
+    .story-language {
+        grid-area: 1 / 6 / 2 / 7;
+        justify-content: flex-end;
+    }
+    
+    .story-summary {
+        grid-area: 2 / 3 / 3 / 7;
         width: 275px;
         max-height: 24px;
+        display: -webkit-box;
+        -webkit-box-orient: vertical;
+        -webkit-line-clamp: 2;
+        line-clamp: 2;
+        overflow: hidden;
     }
 
-    .story-summary p{ 
-        font-size: 8px;
+    .story-playtime {
+        grid-area: 3 / 3 / 4 / 6; 
     }
 
-    .story-meta { 
-        grid-area: 3 / 3 / 4 / 7; 
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-    }
-
-    .story-meta .story-playtime {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        gap: 8px;
-    }
-
-    .story-meta .story-playtime img {
-        height: 18px;
-        width: 18px;
-    }
-
-    .story-meta .story-playtime p {
-        font-size: 8px;
-    }
-
-    .story-meta .story-icons {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        gap: 8px;
-    }
-
-    .story-language { 
-        grid-area: 1 / 6 / 2 / 7;
-        display: flex;
-        align-items: center;
+    .story-icons {
+        grid-area: 3 / 6 / 4 / 7; 
         justify-content: flex-end;
     }
 
-    .story-language img {
-        max-width: 16px;
-        max-height: 12px;
+    .flex-items {
+        display: flex;
+        align-items: center;
+        gap: 8px;
     }
 </style>
