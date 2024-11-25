2b57e8451fa5c975fb6224846878e4ea8ef6d8da
Jesse Kramer <jessekramer15@gmail.com>
2024-11-25 18:23:03 +0100

    JK - Changed pixels to em

--- src/components/story.svelte
+++ src/components/story.svelte
@@ -26,75 +26,75 @@
 
 <style>
     li {
-        width: 360px;
-        max-height: 85px;
-        display: grid;
-        grid-template-columns: repeat(6, auto);
-        grid-template-rows: repeat(3, 1fr);
-        background-color: #fff;
-        overflow: hidden;
-        padding: 10px;
-        border-radius: 4px;
-        color: black;
+      width: 22.5em;
+      max-height: 5.3em;
+      display: grid;
+      grid-template-columns: repeat(6, auto);
+      grid-template-rows: repeat(3, 1fr);
+      background-color: #fff;
+      overflow: hidden;
+      padding: 10px;
+      border-radius: 4px;
+      color: black;
     }
 
     .story-title {
-        font-size: 10px;
-        font-weight: 600;
-        grid-area: 1 / 3 / 2 / 6;
+      font-size: 10px;
+      font-weight: 600;
+      grid-area: 1 / 3 / 2 / 6;
     }
 
     p {
-        font-size: 8px;
+      font-size: 8px;
     }
 
     img {
-        max-width: 18px;
+      max-width: 1.1em;
     }
 
     .story-image {
-        grid-area: 1 / 1 / 4 / 2;
+      grid-area: 1 / 1 / 4 / 2;
     }
 
     .story-image img{
-        max-width: 60px;
-        min-height: 64px;
-        object-fit: cover;
+      max-width: 3.75em;
+      min-height: 4em;
+      object-fit: cover;
     }
 
     .story-language {
-        grid-area: 1 / 6 / 2 / 7;
-        justify-content: flex-end;
+      grid-area: 1 / 6 / 2 / 7;
+      justify-content: flex-end;
     }
     
     .story-summary {
-        grid-area: 2 / 3 / 3 / 7;
-        width: 275px;
-        max-height: 24px;
-        display: -webkit-box;
-        -webkit-box-orient: vertical;
-        -webkit-line-clamp: 2;
-        line-clamp: 2;
-        overflow: hidden;
+      grid-area: 2 / 3 / 3 / 7;
+      width: 275px;
+      max-height: 1.5em;
+      display: -webkit-box;
+      -webkit-box-orient: vertical;
+      -webkit-line-clamp: 2;
+      line-clamp: 2;
+      overflow: hidden;
     }
 
     .story-playtime {
-        grid-area: 3 / 3 / 4 / 6; 
+      grid-area: 3 / 3 / 4 / 6; 
     }
 
     .story-playtime a {
-        display: flex;
-        align-items: center;
+      display: flex;
+      align-items: center;
     }
 
     .story-icons {
-        grid-area: 3 / 6 / 4 / 7; 
-        justify-content: flex-end;
+      grid-area: 3 / 6 / 4 / 7; 
+      justify-content: flex-end;
     }
 
     .flex-items {
-        display: flex;
-        align-items: center;
-        gap: 8px;
+      display: flex;
+      align-items: center;
+      gap: 8px;
     }
 </style>
