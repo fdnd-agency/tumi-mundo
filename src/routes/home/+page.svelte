f43bbfeb5423fecfea1966cdbe901e5f590ab770
lemuelmgsn <magsinolemuel68@gmail.com>
2025-01-08 13:21:52 +0100

    LM - made popupp component
    made popup component with styling

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -16,13 +16,54 @@
             <p>Of your 5-minute goal</p>
         </div>
     </article>
+
     <article>
+        <div class="popup">
+            <p>Goodmorning.</p>
+            <p>Shall we start with a story?</p> 
+            <a href="/">Let's do it!</a>
+        </div>
+        
         <img src="/characters/rat.svg" alt="Rat character"/>
     </article>
     <!-- <Menu/> -->
 </section>
 
 <style>
+
+.popup{
+    display: flex;
+    flex-direction: column;
+    background-color: white;
+    color: black;
+    height: 11.5dvh;
+    width: 55dvw;
+    border-radius: .75rem;
+    padding: .5rem;
+    transform: translateX(-2.5rem);
+}
+.popup > p:nth-of-type(2){
+    padding-bottom: 1rem;
+}
+.popup > a{
+    background-color: #954BE2;
+    color: white;
+    width: 95%;
+    border-radius: 5px;
+    padding: 5px;
+    text-align: center;
+}
+.popup::after{
+    content: '';
+    position: absolute;
+    width: 1.5rem;
+    height: 1.58rem;
+    background-color: white;
+    bottom: -10%;
+    right: 20%;
+    border-radius: 1rem 0;
+}
+/*  */
 section {
     background: url('/home-background.png'), linear-gradient(to bottom, #3F93B7, #1C5872);
     background-size: cover;
@@ -45,7 +86,9 @@ article:first-of-type {
 }
 
 article:nth-of-type(2) { 
-    grid-area: 3 / 1 / 4 / 2; 
+    /* grid-area: 3 / 1 / 4 / 2;  */
+    display: flex;
+    flex-direction: column;
 }
 
 article {
