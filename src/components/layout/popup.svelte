43b21806f17eabea095e0313ce17f98ab5bfedb2
lemuelmgsn <magsinolemuel68@gmail.com>
2025-01-08 15:19:45 +0100

    LM - made popup into component
    made the popup for the home page into component. closes #54

---
+++ src/components/layout/popup.svelte
@@ -0,0 +1,42 @@
+<div class="popup">
+    <p>Goodmorning.</p>
+    <p>Shall we start with a story?</p> 
+    <a href="/lessons">Let's do it!</a>
+</div>
+
+<style>
+
+.popup{
+    display: flex;
+    flex-direction: column;
+    background-color: white;
+    color: black;
+    height: 11.5dvh;
+    width: 55dvw;
+    border-radius: .75rem;
+    padding: .5rem;
+    transform: translateX(-2.5rem);
+}
+.popup > p:nth-of-type(2){
+    padding-bottom: 1rem;
+}
+.popup > a{
+    background-color: #954BE2;
+    color: white;
+    width: 95%;
+    border-radius: 5px;
+    padding: 5px;
+    text-align: center;
+}
+.popup::after{
+    content: '';
+    position: absolute;
+    width: 1.5rem;
+    height: 1.58rem;
+    background-color: white;
+    bottom: -10%;
+    right: 20%;
+    border-radius: 1rem 0;
+}
+
+</style>
--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -3,6 +3,8 @@
 
     import Menu2 from '../../components/core/menu2.svelte';
 
+    import Popup from '../../components/layout/popup.svelte';
+
     let currentPage = "home";
 </script>
 
@@ -18,12 +20,7 @@
     </article>
 
     <article>
-        <div class="popup">
-            <p>Goodmorning.</p>
-            <p>Shall we start with a story?</p> 
-            <a href="/">Let's do it!</a>
-        </div>
-        
+        <Popup/>  
         <img src="/characters/rat.svg" alt="Rat character"/>
     </article>
     <!-- <Menu/> -->
@@ -31,39 +28,6 @@
 
 <style>
 
-.popup{
-    display: flex;
-    flex-direction: column;
-    background-color: white;
-    color: black;
-    height: 11.5dvh;
-    width: 55dvw;
-    border-radius: .75rem;
-    padding: .5rem;
-    transform: translateX(-2.5rem);
-}
-.popup > p:nth-of-type(2){
-    padding-bottom: 1rem;
-}
-.popup > a{
-    background-color: #954BE2;
-    color: white;
-    width: 95%;
-    border-radius: 5px;
-    padding: 5px;
-    text-align: center;
-}
-.popup::after{
-    content: '';
-    position: absolute;
-    width: 1.5rem;
-    height: 1.58rem;
-    background-color: white;
-    bottom: -10%;
-    right: 20%;
-    border-radius: 1rem 0;
-}
-/*  */
 section {
     background: url('/home-background.png'), linear-gradient(to bottom, #3F93B7, #1C5872);
     background-size: cover;
