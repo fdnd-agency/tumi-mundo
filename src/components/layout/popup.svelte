1600a60e1c3566319f3cb2379dcbcf1fcf1ef229
lemuelmgsn <magsinolemuel68@gmail.com>
2025-01-13 22:57:20 +0100

    made popup responsive and finished styling
    used dialog element and made the popup responsive resolves #14

--- src/components/layout/popup.svelte
+++ src/components/layout/popup.svelte
@@ -1,22 +1,4 @@
-<!-- <div class="popup">
-    <p>Goodmorning.</p>
-    <p>Shall we start with a story?</p> 
-    <a href="/lessons">Let's do it!</a>
-</div> -->
-
-<!-- <dialog open class="popup">
-    <form method="dialog">  
-        <button>x</button>
-      </form>
-    <p>Goodmorning.</p>
-    <p>Shall we start with a story?</p>
-    <a href="/lessons">lessons</a>
-  </dialog> -->
-
   <dialog open>
-    <!-- <form method="dialog">
-      <button><img src="/icons/popup-close.svg" alt="close-button"></button>
-    </form> -->
     <div class="popup">
       <div class="heading-close">
         <p>Goodmorning,</p>
@@ -24,52 +6,64 @@
           <button><img src="/icons/popup-close.svg" alt="close-button"></button>
         </form>
       </div>
-        <p>Shall we start with a story?</p> 
+        <p class="popup-message">Shall we start with a story?</p> 
         <a href="/lessons">Let's do it!</a>
     </div>
   </dialog>
 
 
 <style>
-.heading-close{
-  width: 100%;
-  display: flex;
-}
-form{
-  margin-left: auto;
-}
+
 dialog{
     border: none;
     border-radius: .75rem;
-    margin: 0; /* `margin: auto;` is the default. */
+    margin: 0;
     transform: translateX(-50%);
     left: 50%;
+    top: 30%;
 }
+
 button{
   margin-left: auto;
-  padding: .2em;
   border: none;
   background-color: transparent;
 }
+
 .popup{
     display: flex;
     flex-direction: column;
     background-color: white;
     color: black;
-    height: 5rem;
-    width: 60dvw;
+    height: 5em;
+    width: 14em;
     border-radius: .75rem;
     padding: .5rem;
 }
+.heading-close{
+  width: 100%;
+  display: flex;
+}
+
+form{
+  margin-left: auto;
+}
+
 .popup > a{
     background-color: #954BE2;
     color: white;
     width: 95%;
-    /* height: 100%; */
     border-radius: 5px;
-    /* padding: 5px; */
     text-align: center;
 }
+
+.popup > a:hover{
+    background-color: #6619ba;
+}
+
+.popup-message{
+  margin-bottom: .5em;
+}
+
 .popup::after{
     content: '';
     position: absolute;
@@ -81,9 +75,10 @@ button{
     border-radius: 1rem 0;
     z-index: -1;
 }
+
 @media only screen and (min-width: 388px) {
   .popup {
-    width: 55dvw;
+    width: 14em;
   }
 }
 
