5b04a536d38ac31164e134242609181b5066949b
lemuelmgsn <magsinolemuel68@gmail.com>
2025-01-14 23:08:50 +0100

    LM - finised Create own playlist section
    finished create own playlist section. function still needs to be added. resolves #88

--- src/routes/lessons/+page.svelte
+++ src/routes/lessons/+page.svelte
@@ -9,21 +9,26 @@
 
 <main>
 
-<h1>Lessons</h1>
-
-<section>
-    <h2>Own playlist</h2>
+<header>
+    <h1>Lessons</h1>
 
     <ul>
-        <li>
-            <img>
+        <li>1. Listening</li>
+    </ul>
+</header>
+
+<section class="own-playlist">
+    <h2>Own playlists</h2>
+    <ul>
+        <li class="playlist-1">
+            <img src="/temp/temp-story.svg" class="story-img">
             <h3>The octopus who was very hungry</h3>
-            <a>31 min. 55 sec</a>
+            <small><img src="/icons/playlist-playtime.svg" alt="Icon for playtime" height="17.59" width="17.59"><a href="all-stories">31 min. 55 sec</a></small>
         </li>
-        <li>
-            <img>
+        <li class="create-playlist">
+            <img src="/icons/create-playlist.svg">
             <h3>Make a playlist !</h3>
-            <a>Add your favorite stories in one playlist</a>
+            <small><a href="/create-playlist">Add your favorite stories in one playlist</a></small>
         </li>
     </ul>
 </section>
@@ -39,7 +44,7 @@
 
     <ul>
         {#each data.stories as story}
-            <Story {story} />
+            <!-- <Story {story} /> -->
         {/each}
     </ul>
 
@@ -48,11 +53,100 @@
 </main>
 
 <style>
+h1{
+    margin-top: 2rem;
+    margin-bottom: 1rem;
+}
+h2{
+    font-size: 1.3em;
+}
+header{
+    margin-bottom: 2rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+h2{
+    margin-bottom: .75rem;
+}
+.create-playlist, .playlist-1{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+.playlist-1 > img{
+    width: 100%;
+}
+.playlist-1{
+    background-color: white;
+}
+.playlist-1 > a small{
+    color: black;
+}
+.playlist-1 > h3{
+    font-weight: bold;
+}
+.own-playlist{
+    display: flex;
+    flex-direction: column;
+}
+.own-playlist > ul{
+    display: flex;
+    gap: 10px;
+    margin-bottom: 5dvh;
+}
+.own-playlist > ul > li{
+    height: 12.5rem;
+    width: 9rem;
+    color: black;
+    border-radius: .5rem;
+    padding: .7em;
+}
+.create-playlist{
+    background-color: #494375;
+    text-align: center;
+    color: white;
+}
+.create-playlist > h3{
+    margin-bottom: 1em;
+    color: white;
+    font-weight: bold;
+    font-size: 1em;
+}
+.create-playlist > a{
+    color: white;
+    font-size: 1em;
+}
+.create-playlist > small{
+    color: white;
+}
+:root{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
 
 main{
     height: 100dvh;
     color: white;
     background-image: linear-gradient(#471871, #142151);
+    display: flex;
+    flex-direction: column;
+    /* align-items: center; */
+}
+section{
+    padding: 1rem;
+}
+.playlist-1 > small{
+    align-self: start;
+    margin-top: auto;
+}
+small img {
+    vertical-align: middle;
+    padding-right: .3em;
+    height: 2em;
+    width: 2em;
 }
 
 </style>
--- src/routes/log-in/+page.svelte
+++ src/routes/log-in/+page.svelte
@@ -65,12 +65,12 @@ section, .buttons {
 }
 
 main {
-    background-image: url(/log-in-bg.svg);
-    background-size: contain;
+    background: url(/log-in-bg.svg), linear-gradient(to bottom, #3F93B7, #1C5872);
+    background-size: cover;
     background-repeat: no-repeat;
     background-position: bottom;
     height: 100vh;
-    height: 100vh; /* for dynamic viewport height */
+    height: 100dvh; /* for dynamic viewport height */
     width: 100%;
     color: white;
     font-family: Poppins, sans-serif;
@@ -82,7 +82,7 @@ section {
 }
 
 h1 {
-    margin-top: 45%;
+    margin-top: 10rem;
     font-size: 3rem;
     font-weight: bold;
     margin-bottom: .5rem;
@@ -104,7 +104,7 @@ button, .popup-button, .login-popup, .sign-up{
     border: 1px solid white;
     border-radius: 10px;
     padding: .8rem;
-    width: 100%;
+    max-width: 20rem;
     background-color: #378DB3;
     color: white;
     text-align: center;
@@ -126,7 +126,7 @@ button:hover, .popup-button:hover {
 a{
     color: #378EB4;
     text-decoration: underline;
-    width: 100%;
+    max-width: 22rem;
     text-align: center;
 }
 /* styling for popup heading */
@@ -183,6 +183,7 @@ input{
 }
 label{
     margin-bottom: 10px;
+    max-width: 20rem;
 }
 
 /* Styling for popup */
