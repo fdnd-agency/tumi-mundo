b84789b92a6a6504eae1b9e21cc54abd6b4c7832
lemuelmgsn <magsinolemuel68@gmail.com>
2025-08-17 01:53:30 +0200

    refactor: moved like function to component
    resolves #363

---
+++ src/lib/components/core/PlaylistLike.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { OwnPlaylists, LikedPlaylists, SuggestedPlaylists } from '$lib/index';
+  export let data;
+
+  function handleLikeToggle(event) {
+    const { playlistId, isLiked } = event.detail;
+    data.playlists = data.playlists.map(playlist =>
+      playlist.id === playlistId
+        ? { ...playlist, isLiked }
+        : playlist
+    );
+  }
+</script>
+
+<OwnPlaylists {data} on:likeToggle={handleLikeToggle} />
+<LikedPlaylists {data} on:likeToggle={handleLikeToggle} />
+<SuggestedPlaylists {data} on:likeToggle={handleLikeToggle} />
