3677c4fe4e2b1f46fcb824d6e6eb5d20bf2da8f7
lemuelmgsn <magsinolemuel68@gmail.com>
2025-08-19 01:30:35 +0200

    feature: added Play story function
    resolves #200

--- src/lib/components/core/PlaylistMeta.svelte
+++ src/lib/components/core/PlaylistMeta.svelte
@@ -6,6 +6,8 @@
   let existingLikeId = playlist?.likeId || null;
   let error = null;
   let profileId = 122; 
+  $: firstStoryId = playlist?.stories?.[0]?.id || playlist?.stories?.[0]?.story?.id;
+
 
   async function toggleLike(event) {
     event.preventDefault();
@@ -26,6 +28,7 @@
       error = err.message || 'Er is iets fout gegaan';
     }
   }
+
 </script>
 
 <section class="meta-section">
@@ -53,7 +56,16 @@
         <path d="M11.6536 7.15238C11.8471 7.33832 12.1529 7.33832 12.3464 7.15238C13.1829 6.34871 14.326 5.75 15.6 5.75C18.1489 5.75 20.25 7.64769 20.25 10.0298C20.25 11.7261 19.4577 13.1809 18.348 14.428C17.2397 15.6736 15.7972 16.7316 14.4588 17.6376L12.1401 19.207C12.0555 19.2643 11.9445 19.2643 11.8599 19.207L9.54125 17.6376C8.20278 16.7316 6.76035 15.6736 5.65201 14.428C4.54225 13.1809 3.75 11.7261 3.75 10.0298C3.75 7.64769 5.85106 5.75 8.4 5.75C9.67403 5.75 10.8171 6.34871 11.6536 7.15238Z" stroke="#C4C4C4" stroke-linecap="round" stroke-linejoin="round"/>
       </svg>
     </button>
-    <a href="/play"><img src="/icons/play.svg" alt="play" height="60" /></a>
+  
+    {#if firstStoryId}
+      <a href={`/story/${firstStoryId}`} aria-label="Play first story">
+        <img src="/icons/play.svg" alt="play" height="60" />
+      </a>
+    {:else}
+      <button class="play" disabled aria-disabled="true" title="No stories in this playlist">
+        <img src="/icons/play.svg" alt="play (disabled)" height="60" />
+      </button>
+    {/if}
   </div>
 
   {#if error}
